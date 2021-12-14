a5fa437a736c4f152b12a58adb02e84f588372f0
Robin <RobinAndrews22@gmail.com>
2021-12-14 09:56:20 -0600

    commit tree 65fb7fc6e0b7cd61432e6e75e26f216b5845229b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun("watch a movie");
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+mondayWork();
+
+const wrapAdjective = function (adj = '*') {
+    return function (word = "special") {
+      return `You are ${adj}${word}${adj}!`;
+    };
+  }
+  wrapAdjective("||")("a dedicated programmer");
